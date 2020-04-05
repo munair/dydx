b66cd29628851155f393d898b90bb96d70ab0070
Munair Simpson <munair@gmail.com>
2020-04-05 01:36:57 -0500

    breakout balances and use print-f with a float (not precision) to format the values.

--- get-dydx-balances.py
+++ get-dydx-balances.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from decimal import Decimal
+
 from dydx.client import Client
 import dydx.constants as consts
 import dydx.util as utils
@@ -7,7 +9,15 @@ import dydx.util as utils
 from credentials import client
 
 
-# get dYdX balances
+# Get dYdX account balances
 balances = client.eth.get_my_balances()
 
-print ( balances )
+# Disaggregate asset balances
+ethbalance = Decimal(balances[0] / (10**18))
+usdbalance = Decimal(balances[2] / (10**6))
+daibalance = Decimal(balances[3] / (10**18))
+
+# Display dYdX account balance information
+print (f'{ethbalance:28.18f} ETH')
+print (f'{usdbalance:28.18f} USD')
+print (f'{daibalance:28.18f} DAI')
