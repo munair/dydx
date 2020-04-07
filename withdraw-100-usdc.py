e982bd180cc852dd1306f2c3bac0dc7257ff300d
Munair Simpson <munair@gmail.com>
2020-04-06 20:34:40 -0500

    finally confirmed that this script works (and cleaned up a little for readability).

--- withdraw-100-usdc.py
+++ withdraw-100-usdc.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from decimal import Decimal
+
 from dydx.client import Client
 import dydx.constants as consts
 import dydx.util as utils
@@ -7,10 +9,13 @@ import dydx.util as utils
 from credentials import client
 
 
-# Withdraw 100 USDC
+# Specify withdraw amount
+withdrawalamount = Decimal("100")
+
+# Withdraw USDC
 tx_hash = client.eth.withdraw(
   market=consts.MARKET_USDC,
-  wei=utils.token_to_wei(100, consts.MARKET_USDC)
+  wei=utils.token_to_wei(withdrawalamount, consts.MARKET_USDC)
 )
 receipt = client.eth.get_receipt(tx_hash)
 
