744c7a0de567949a71e317dcb7eceb7495ee22fa
Munair Simpson <munair@gmail.com>
2020-04-06 20:46:10 -0500

    Most likely we will only depost 100 bucks at a time to the smart contracts. So let us make the naming reflect that custom.

--- deposit-100-usdc.py
+++ deposit-100-usdc.py
@@ -10,10 +10,10 @@ from credentials import client
 
 
 # Set Allowance [must only be called once EVER]
-# tx_hash = client.eth.set_allowance(market=consts.MARKET_USDC)
-# receipt = client.eth.get_receipt(tx_hash)
+tx_hash = client.eth.set_allowance(market=consts.MARKET_USDC)
+receipt = client.eth.get_receipt(tx_hash)
 
-depositamount = Decimal("1")
+depositamount = Decimal("100")
 
 # Deposit USDC
 tx_hash = client.eth.deposit(
