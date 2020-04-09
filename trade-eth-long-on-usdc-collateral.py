bcb15934068e5d5208c9309f9bac4e0fed236960
Munair Simpson <munair@gmail.com>
2020-04-09 18:25:50 -0500

    second draft.

--- trade-eth-long-on-usdc-collateral.py
+++ trade-eth-long-on-usdc-collateral.py
@@ -12,12 +12,17 @@ from credentials import client
 
 
 # Define the return on assets and price drop (pricetrigger) required for bidding
+# Also define a stop limit ask and a stop market ask (just in case the price crashes)
 # In addition, define target (mimimum) collateralization ratio and maximum leverage
 # Note: to make a bid without waiting for the prices to fall, set the price trigger to 1
 pricetrigger = Decimal( "0.99" )
 # Submit a bid after a 1% drop in the ask price
 requiredreturn = Decimal( "1.01" )
-# Submit an ask after a 1% appreciation in the bid price from the price paid for ETH.
+# Submit an ask immediately after the bid is filled.
+stoplimitask = Decimal( "0.99" )
+# Break out of a loop waiting for the profitable ask to fill if the stop limit is triggered.
+stopmarketask = Decimal( "0.98" )
+# Break out of a loop waiting for the profitable ask to fill if the stop market is triggered.
 maximumleverage = Decimal( "5" )
 # 5X constants established by dYdX (note that it is 4X for a SHORT)
 minimumcollateralization = Decimal( "1.25" )
@@ -134,7 +139,7 @@ while True:
     # Give the bid placed five seconds to fill
     time.sleep(5)
 
-    # Get fills created by my account for both sides of the orderbook
+    # Get fills
     my_fills = client.get_my_fills(
         market=['WETH-DAI'],
         limit=1
@@ -159,3 +164,61 @@ placed_ask = client.place_order(
 # Display order information
 jsondata = json.dumps( placed_ask, sort_keys=True, indent=4, separators=(',', ': ') )
 print ( jsondata )
+
+while True:
+    # Check the status of the submitted ask
+    submittedask = client.get_order( orderId=placed_ask["order"]["id"] )
+    if submittedask["order"]["status"] == "FILLED":
+        # Exit: End the loop and exit.
+        print ("Order ", submittedask["order"]["id"], " was filled at: ", submittedask["order"]["price"], " DAI/ETH.")
+        break
+    else:
+        # Sleep
+        # Then check price
+        time.sleep(5)
+        bookprices = bestprices( 'WETH-DAI', daiquotetick )
+        bookmarket = bookprices[1]
+        limitprice = bookprices[2]
+        # If the present price is below the trigger price this loop ends
+        if Decimal( bookmarket ) < Decimal( greatestbid ) * Decimal( stopmarketask ):
+            # Create order to SELL ETH
+            placed_ask = client.place_order(
+                market=consts.PAIR_WETH_DAI,
+                side=consts.SIDE_SELL,
+                amount=utils.token_to_wei(quantity, consts.MARKET_WETH),
+                price=bookmarket.quantize( Decimal(daiquotetick) ),
+                fillOrKill=False,
+                postOnly=False
+            )
+            # Display order information
+            jsondata = json.dumps( placed_ask, sort_keys=True, indent=4, separators=(',', ': ') )
+            print ( jsondata )
+            # Cancel the previously submitted ask then exit the loop.
+            print ( "Cancelling order: ", submittedask["order"]["id"] )
+            canceledask = client.cancel_order( hash=my_order["order"]["id"] )
+            # Display order cancel information
+            jsondata = json.dumps( canceledask, sort_keys=True, indent=4, separators=(',', ': ') )
+            print ( jsondata )
+            # exit loop
+            break
+        elif Decimal( bookmarket ) < Decimal( greatestbid ) * Decimal( stoplimitask ):
+            # Create order to SELL ETH
+            placed_ask = client.place_order(
+                market=consts.PAIR_WETH_DAI,
+                side=consts.SIDE_SELL,
+                amount=utils.token_to_wei(quantity, consts.MARKET_WETH),
+                price=limitprice.quantize( Decimal(daiquotetick) ),
+                fillOrKill=False,
+                postOnly=False
+            )
+            # Display order information
+            jsondata = json.dumps( placed_ask, sort_keys=True, indent=4, separators=(',', ': ') )
+            print ( jsondata )
+            # Cancel the previously submitted ask then exit the loop.
+            print ( "Cancelling order: ", submittedask["order"]["id"] )
+            canceledask = client.cancel_order( hash=my_order["order"]["id"] )
+            # Display order cancel information
+            jsondata = json.dumps( canceledask, sort_keys=True, indent=4, separators=(',', ': ') )
+            print ( jsondata )
+            # exit loop
+            break
